9488ff4b75dd310378397b18bbf9b9a6e383ed65
jaimegarza <jaimegarza@gmail.com>
2012-06-27 10:35:10 -0700

    Added comments to the root package

--- src/main/java/me/jaimegarza/syntax/OutputException.java
+++ src/main/java/me/jaimegarza/syntax/OutputException.java
@@ -28,14 +28,45 @@
 */
 package me.jaimegarza.syntax;
 
+/**
+ * This Exception is a generic wrapping exception that is thrown when the output
+ * phase has an error.
+ * 
+ * The output phase is the one where the code is emitted and completed.
+ *
+ * @author jaimegarza@gmail.com
+ *
+ */
+
 public class OutputException extends Exception {
 
   private static final long serialVersionUID = 3894726711472166627L;
 
+  /**
+   * Constructs a new output exception with the specified detail message and
+   * cause.  <p>Note that the detail message associated with
+   * <code>cause</code> is <i>not</i> automatically incorporated in
+   * this exception's detail message.
+   *
+   * @param  message the detail message (which is saved for later retrieval
+   *         by the {@link #getMessage()} method).
+   * @param  cause the cause (which is saved for later retrieval by the
+   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         permitted, and indicates that the cause is nonexistent or
+   *         unknown.)
+   */
   public OutputException(String message, Throwable cause) {
     super("parsing error:" + message, cause);
   }
 
+  /**
+   * Constructs a new output exception with the specified detail 
+   * message.  The cause is not initialized, and may subsequently be initialized
+   * by a call to {@link #initCause}.
+   *
+   * @param   message   the detail message. The detail message is saved for 
+   *          later retrieval by the {@link #getMessage()} method.
+   */
   public OutputException(String message) {
     super(message);
   }
