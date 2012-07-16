090fe14d28480ffc3033072f09e2210a935777e9
jaimegarza <jaimegarza@gmail.com>
2012-07-16 08:44:12 -0700

    RuntimeData comments. Done with bulk commenting for now.

--- src/main/java/me/jaimegarza/syntax/generator/RuntimeData.java
+++ src/main/java/me/jaimegarza/syntax/generator/RuntimeData.java
@@ -38,23 +38,73 @@ import me.jaimegarza.syntax.definition.Symbol;
 import me.jaimegarza.syntax.definition.Terminal;
 import me.jaimegarza.syntax.definition.Type;
 
+/**
+ * Class that is used to hold the state of the phases.
+ *
+ * @author jaimegarza@gmail.com
+ *
+ */
 public class RuntimeData {
+  /**
+   * Starting symbol as computed.  The source of truth at the end of parsing
+   */
   private NonTerminal root;
+  /**
+   * Starting symbol as given
+   */
   private NonTerminal start;
-
+  /**
+   * List of all rules
+   */
   private List<Rule> rules = new LinkedList<Rule>();
+  /**
+   * List of non terminals
+   */
   private List<NonTerminal> nonTerminals = new LinkedList<NonTerminal>();
+  /**
+   * list of terminal symbols, or tokens
+   */
   private List<Terminal> terminals = new LinkedList<Terminal>();
+  /**
+   * List of types
+   */
   private List<Type> types = new LinkedList<Type>();
+  /**
+   * List of states
+   */
   private State[] I;
+  /**
+   * How many gotos are there in the goto table?
+   */
   private int numberOfGoTos;
+  /**
+   * How many actions are there in the action table?
+   */
   private int numberOfActions;
+  /**
+   * How many errors are there in the error table?
+   */
   private int numberOfErrors;
+  /**
+   * How many elements are there in the recovery table?
+   */
   private int numberOfRecoveries;
+  /**
+   * List of error messages as computed
+   * TODO: allow assignment of errors per rule?
+   */
   private List<String> errorMessages;
+  /**
+   * Does it have final actions?
+   */
   private boolean finalActions;
-
+  /**
+   * Current line number in the source file
+   */
   public int sourceLineNumber = 0;
+  /**
+   * Did the stack structure get defined?
+   */
   private boolean stackTypeDefined;
   
   /**
@@ -73,58 +123,12 @@ public class RuntimeData {
     return false;
   }
 
-  public NonTerminal getRoot() {
-    return root;
-  }
-
-  public void setRoot(NonTerminal root) {
-    this.root = root;
-  }
-
-  public NonTerminal getStart() {
-    return start;
-  }
-
-  public void setStart(NonTerminal start) {
-    this.start = start;
-  }
-
-  public List<Rule> getRules() {
-    return rules;
-  }
-
-  public List<NonTerminal> getNonTerminals() {
-    return nonTerminals;
-  }
-
-  public List<Terminal> getTerminals() {
-    return terminals;
-  }
-
-  public int getNumberOfGoTos() {
-    return numberOfGoTos;
-  }
-
-  public void setNumberOfGoTos(int numberOfGoTos) {
-    this.numberOfGoTos = numberOfGoTos;
-  }
-
-  public List<String> getErrorMessages() {
-    return errorMessages;
-  }
-
-  public void setErrorMessages(List<String> errorMessages) {
-    this.errorMessages = errorMessages;
-  }
-
-  public State[] getStates() {
-    return I;
-  }
-
-  public void setStates(State[] I) {
-    this.I = I;
-  }
-
+  /**
+   * Generic routine to find symbols
+   * @param table the table to look into
+   * @param name the name of the symbol
+   * @return the symbol, or null if not found.
+   */
   public <T extends Symbol> T findSymbolByName(List<T> table, String name) {
     for (T symbol : table) {
       if (symbol.getName().equals(name)) {
@@ -135,6 +139,12 @@ public class RuntimeData {
     return null;
   }
 
+  /**
+   * Generic routine to find symbols
+   * @param table the table to look into
+   * @param tokenId the token of the symbol
+   * @return the symbol, or null if not found.
+   */
   public <T extends Symbol> T findSymbolByToken(List<T> table, int tokenId) {
     for (T symbol : table) {
       if (symbol.getToken() == tokenId) {
@@ -144,6 +154,12 @@ public class RuntimeData {
     return null;
   }
 
+  /**
+   * Generic routine to find symbols
+   * @param table the table to look into
+   * @param id the id of the symbol
+   * @return the symbol, or null if not found.
+   */
   public <T extends Symbol> T findSymbolById(List<T> table, int id) {
     for (T symbol : table) {
       if (symbol.getId() == id) {
@@ -153,30 +169,65 @@ public class RuntimeData {
     return null;
   }
 
+  /**
+   * Find one terminal by name
+   * @param name the name of the symbol
+   * @return the terminal, or null if not found
+   */
   public Terminal findTerminalByName(String name) {
     return findSymbolByName(terminals, name);
   }
 
+  /**
+   * Find one terminal by token
+   * @param tokenId the token of the symbol
+   * @return the terminal, or null if not found
+   */
   public Terminal findTerminalByToken(int tokenId) {
     return findSymbolByToken(terminals, tokenId);
   }
 
+  /**
+   * Find one terminal by id
+   * @param id the id of the symbol
+   * @return the terminal, or null if not found
+   */
   public Terminal findTerminalById(int id) {
     return findSymbolById(terminals, id);
   }
 
+  /**
+   * Find one non terminal by name
+   * @param name the name of the symbol
+   * @return the terminal, or null if not found
+   */
   public NonTerminal findNonTerminalByName(String name) {
     return findSymbolByName(nonTerminals, name);
   }
 
+  /**
+   * Find one non terminal by token
+   * @param tokenId the token of the symbol
+   * @return the terminal, or null if not found
+   */
   public NonTerminal findNonTerminalByToken(int tokenId) {
     return findSymbolByToken(nonTerminals, tokenId);
   }
 
+  /**
+   * Find one non terminal by id
+   * @param id the id of the symbol
+   * @return the terminal, or null if not found
+   */
   public NonTerminal findNonTerminalById(int id) {
     return findSymbolById(nonTerminals, id);
   }
 
+  /**
+   * Locate a type
+   * @param name the name of the type
+   * @return the type, or null if not found
+   */
   public Type findType(String name) {
     for (Type t : types) {
       if (t.getName().equals(name)) {
@@ -186,48 +237,214 @@ public class RuntimeData {
     return null;
   }
 
+  /**
+   * @return the root
+   */
+  public NonTerminal getRoot() {
+    return root;
+  }
+
+  /**
+   * @param root the root to set
+   */
+  public void setRoot(NonTerminal root) {
+    this.root = root;
+  }
+
+  /**
+   * @return the start
+   */
+  public NonTerminal getStart() {
+    return start;
+  }
+
+  /**
+   * @param start the start to set
+   */
+  public void setStart(NonTerminal start) {
+    this.start = start;
+  }
+
+  /**
+   * @return the rules
+   */
+  public List<Rule> getRules() {
+    return rules;
+  }
+
+  /**
+   * @param rules the rules to set
+   */
+  public void setRules(List<Rule> rules) {
+    this.rules = rules;
+  }
+
+  /**
+   * @return the nonTerminals
+   */
+  public List<NonTerminal> getNonTerminals() {
+    return nonTerminals;
+  }
+
+  /**
+   * @param nonTerminals the nonTerminals to set
+   */
+  public void setNonTerminals(List<NonTerminal> nonTerminals) {
+    this.nonTerminals = nonTerminals;
+  }
+
+  /**
+   * @return the terminals
+   */
+  public List<Terminal> getTerminals() {
+    return terminals;
+  }
+
+  /**
+   * @param terminals the terminals to set
+   */
+  public void setTerminals(List<Terminal> terminals) {
+    this.terminals = terminals;
+  }
+
+  /**
+   * @return the types
+   */
   public List<Type> getTypes() {
     return types;
   }
 
-  public boolean isStackTypeDefined() {
-    return stackTypeDefined;
+  /**
+   * @param types the types to set
+   */
+  public void setTypes(List<Type> types) {
+    this.types = types;
   }
 
-  public void setStackTypeDefined(boolean stackTypeDefined) {
-    this.stackTypeDefined = stackTypeDefined;
+  /**
+   * @return the i
+   */
+  public State[] getStates() {
+    return I;
   }
 
+  /**
+   * @param i the i to set
+   */
+  public void setStates(State[] i) {
+    I = i;
+  }
+
+  /**
+   * @return the numberOfGoTos
+   */
+  public int getNumberOfGoTos() {
+    return numberOfGoTos;
+  }
+
+  /**
+   * @param numberOfGoTos the numberOfGoTos to set
+   */
+  public void setNumberOfGoTos(int numberOfGoTos) {
+    this.numberOfGoTos = numberOfGoTos;
+  }
+
+  /**
+   * @return the numberOfActions
+   */
   public int getNumberOfActions() {
     return numberOfActions;
   }
 
+  /**
+   * @param numberOfActions the numberOfActions to set
+   */
   public void setNumberOfActions(int numberOfActions) {
     this.numberOfActions = numberOfActions;
   }
 
+  /**
+   * @return the numberOfErrors
+   */
   public int getNumberOfErrors() {
     return numberOfErrors;
   }
 
+  /**
+   * @param numberOfErrors the numberOfErrors to set
+   */
   public void setNumberOfErrors(int numberOfErrors) {
     this.numberOfErrors = numberOfErrors;
   }
 
+  /**
+   * @return the numberOfRecoveries
+   */
   public int getNumberOfRecoveries() {
     return numberOfRecoveries;
   }
 
+  /**
+   * @param numberOfRecoveries the numberOfRecoveries to set
+   */
   public void setNumberOfRecoveries(int numberOfRecoveries) {
     this.numberOfRecoveries = numberOfRecoveries;
   }
 
+  /**
+   * @return the errorMessages
+   */
+  public List<String> getErrorMessages() {
+    return errorMessages;
+  }
+
+  /**
+   * @param errorMessages the errorMessages to set
+   */
+  public void setErrorMessages(List<String> errorMessages) {
+    this.errorMessages = errorMessages;
+  }
+
+  /**
+   * @return the finalActions
+   */
   public boolean hasFinalActions() {
     return finalActions;
   }
 
+  /**
+   * @param finalActions the finalActions to set
+   */
   public void setFinalActions(boolean finalActions) {
     this.finalActions = finalActions;
+  }
+
+  /**
+   * @return the sourceLineNumber
+   */
+  public int getSourceLineNumber() {
+    return sourceLineNumber;
+  }
+
+  /**
+   * @param sourceLineNumber the sourceLineNumber to set
+   */
+  public void setSourceLineNumber(int sourceLineNumber) {
+    this.sourceLineNumber = sourceLineNumber;
+  }
+
+  /**
+   * @return the stackTypeDefined
+   */
+  public boolean isStackTypeDefined() {
+    return stackTypeDefined;
+  }
+
+  /**
+   * @param stackTypeDefined the stackTypeDefined to set
+   */
+  public void setStackTypeDefined(boolean stackTypeDefined) {
+    this.stackTypeDefined = stackTypeDefined;
   }
 
 }
